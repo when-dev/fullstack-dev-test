623328bf14a5e16275b1a9d6974b1621cc123ecb
Egor Gukov <108114245+when-dev@users.noreply.github.com>
2025-06-10 19:59:00 +0300

    feat: rendered CryptoItem from mock data

--- frontend/src/components/cryptoList/CryptoList.jsx
+++ frontend/src/components/cryptoList/CryptoList.jsx
@@ -1,14 +1,20 @@
-import styles from "./CryptoList.module.css"
-import CryptoItem from "../cryptoItem/CryptoItem"
+import styles from './CryptoList.module.css'
+import CryptoItem from '../cryptoItem/CryptoItem'
+
+const mockItems = [
+	{ id: 'btc', name: 'Bitcoin', priceUsd: '109000' },
+	{ id: 'eth', name: 'Ethereum', priceUsd: '2700' },
+]
+
+const mockColors = ['#ff0000', '#00ff00']
 
 const CryptoList = () => {
-	
 	return (
 		<div className={styles.listContainer}>
-			<CryptoItem item={''} color="red" />
-			<div className={styles.loader}>
-				loader
-			</div>
+			{mockItems.map((item, idx) => (
+				<CryptoItem key={item.id} item={item} color={mockColors[idx]} />
+			))}
+			<div className={styles.loader}>loader</div>
 		</div>
 	)
 }
